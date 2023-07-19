b2ac9c667015f027523d377ef5be8997105a6ff0
adiraijamal <106314411+adiraijamal@users.noreply.github.com>
2023-07-19 16:33:57 +0400

    git
    git

--- purchases/models.py
+++ purchases/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from products.models import Product
 from vendors.models import Vendor
 
+
 class Purchase(models.Model):
     cashier = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='purchases')
@@ -12,4 +13,9 @@ class Purchase(models.Model):
     quantity = models.PositiveIntegerField()
 
     def __str__(self):
-        return f"Cashier: {self.cashier.username}, Product: {self.product.name}, Vendor: {self.vendor.name}, Purchase Date: {self.purchase_date}, Cost Price: {self.cost_price}, Quantity: {self.quantity}"
+        return f"Cashier: {self.cashier.name}\n"\
+               f"Product: {self.product.name}\n"\
+               f"Vendor: {self.vendor.name}\n"\
+               f"Purchase Date: {self.purchase_date}\n" \
+               f"Cost Price: {self.cost_price}\n"\
+               f"Quantity: {self.quantity}"
